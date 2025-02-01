a3df12f68d0d2a2aa679cc7c25332699ed528597
Owen A <owen.allemang@gmail.com>
2025-02-01 16:11:29 +0000

    notes and todos of changes to make

--- fuse_v2/example_input_files/mlgen_chgnet_input.py
+++ fuse_v2/example_input_files/mlgen_chgnet_input.py
@@ -39,11 +39,13 @@ run_fuse(
 	# gn boss model for structure generation
 	generate_gn_boss_structures=True,
 	# if set to true, when FUSE is firt launched, it will run gn-boss to generate the pool of referennce structures for this calculation.
-	gn_boss_command=os.environ['CONDA'] + r"conda run -p" + os.environ['GNBOSS'] + "get_cifs_for_FUSE.py",
+	# TODO: Conda is not in PATH ~ not always recommended
+	gn_boss_command=os.system(f"mamba run -n gnboss get_cifs_for_FUSE.py"),
+
 	# For my machine, I've setup gn-boss in a seperate python environment, this is the command for that version of python
 	gn_search='tpe',  # 'rand' random search, 'tpe' baysian opt, 'pso' particle swarm
 	gn_max_step=5000,  # number of generation attempts for gn-boss
-	gn_template_path=os.environ['GNBOSS_TEMP'],  # path to template files for using gn-boss
+	gn_template_path="E:\\PycharmProjects\\keho\\FUSE-stable\\fuse_v2\\gnboss_template",  # path to template files for using gn-boss
 	gn_zn_range=[1, 2, 3, 4],  # numbers of formula units to scan with GN-BOSS for generating structures
 	rank_gn_structures='single',
 	# if None; do not rank structures, this should only be set if pull_random = True above, if 'opti' rank with SPPs AFTER geometry optimising the, if 'sing' rank based on single point calculations with SPPs.
@@ -105,7 +107,7 @@ run_fuse(
 	shel=[''],  # species using shells in gulp
 	gulp_timeout='',  # timout command for running gulp, leave as '' if not using, only use in Windows!
 	assemble_spp_=True,  # If set to true collate SPP potential for the system
-	spp_path=os.environ['SPP_PATH'],
-	gulp_command=os.environ['ASE_GULP_COMMAND'],
+	spp_path="E:\\PycharmProjects\\keho\\FUSE-stable\\fuse_v2\\SPP",
+	# gulp_command=os.environ['ASE_GULP_COMMAND'],
 
 )
