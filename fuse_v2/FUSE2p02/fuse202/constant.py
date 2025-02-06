1fad4d3e502a717042cab5d268b3c0e790ff134a
Owen A <owen.allemang@gmail.com>
2025-02-06 18:37:52 +0000

    created constant file

---
+++ fuse_v2/FUSE2p02/fuse202/constant.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+
+
+from fuse202.utils.directories import cache_solutions_dir
+
+SPP_PATH = r'C:\Users\cc0u5\Documents\SPP_library\SPP'
+SOLUTIONS_DIR = cache_solutions_dir()
+
+
+Ry_TO_eV = 13.605693123     # wolframAlpha
