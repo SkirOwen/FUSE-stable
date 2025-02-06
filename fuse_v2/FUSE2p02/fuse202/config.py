9d91ab1884a9a9528600f6b1593468f45c5bee0b
Owen A <owen.allemang@gmail.com>
2025-02-06 18:37:37 +0000

    added config file and function to get abspath of the fuse202 parent dir

--- fuse_v2/FUSE2p02/fuse202/config.py
+++ fuse_v2/FUSE2p02/fuse202/config.py
@@ -1,2 +1,12 @@
-SPP_PATH = r'C:\Users\cc0u5\Documents\SPP_library\SPP'
-SOLUTIONS_DIR = ""
+import inspect
+import importlib
+import os
+
+
+def get_fuse202_dir():
+	fuse202_module = importlib.import_module("fuse202")
+	fuse202_dir = os.path.dirname(inspect.getabsfile(fuse202_module))
+	return os.path.abspath(os.path.join(fuse202_dir, ".."))
+
+
+
